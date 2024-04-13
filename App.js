79014db461744de635621afd718bf4586e0442ba
Mukelani <131559676+MukelaniGumaMashoba@users.noreply.github.com>
2024-04-13 12:15:53 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Calculator from './Screens/Calculator.js';
-import Third from './Screens/Third.js';
-import Second from './Screens/Second.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -11,12 +9,7 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: '#3498db',
-          inactiveTintColor: 'gray',
-        }}
-      >
+      <Tab.Navigator>
         <Tab.Screen
           name="Calculator"
           component={Calculator}
@@ -24,26 +17,6 @@ export default function App() {
             tabBarLabel: 'Calculator',
             tabBarIcon: ({ color, size }) => (
               <FontAwesome5 name="calculator" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Second"
-          component={Second}
-          options={{
-            tabBarLabel: 'Second',
-            tabBarIcon: ({ color, size }) => (
-              <FontAwesome5 name="arrow-left" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Third"
-          component={Third}
-          options={{
-            tabBarLabel: 'Third',
-            tabBarIcon: ({ color, size }) => (
-              <FontAwesome5 name="arrow-left" size={size} color={color} />
             ),
           }}
         />
